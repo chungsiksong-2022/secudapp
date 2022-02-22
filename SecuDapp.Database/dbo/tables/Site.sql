3f2076c1545bb21e3b87929c357511c3ab1900d3
danielsong-2021 <daniel.song@portsamerica.com>
2022-02-21 16:06:09 -0800

    New SQL database project

---
+++ SecuDapp.Database/dbo/tables/Site.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Terminal] (
+    [Id]                       UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [UserIdCreation]           UNIQUEIDENTIFIER NOT NULL,
+    [UserIdUpdate]             UNIQUEIDENTIFIER NULL,
+    [BrandingId]               UNIQUEIDENTIFIER NULL,
+    [CreationTime]             DATETIME2 (7)    DEFAULT (getdate()) NOT NULL,
+    [UpdateTime]               DATETIME2 (7)    NULL,
+    [Active]                   BIT              DEFAULT ((1)) NOT NULL,
+    [Name]                     NVARCHAR (200)   NOT NULL,
+    [IconUrl]                  NVARCHAR (2000)  NULL,
+    [AutomaticApprovalEnabled] BIT              DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Terminal] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
